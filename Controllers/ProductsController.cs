1e8fd9fa3b65d98164dcd3127c5754377529eded
Ivan Martsilenko <ivan.martsilenko@unicyb.kiev.ua>
2020-09-13 12:23:37 +0300

    Change name in launch settings to WebApiSample add methods get and get id added singleton ( DataBase)

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.ControllersS
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        DataBase _db;
+
+        public ProductsController(DataBase db)
+        {
+            this._db = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _db;
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "undone";
+        }
+    }
+}
--- DataBase.cs
+++ DataBase.cs
@@ -13,5 +13,6 @@ namespace API
     }
     public class DataBase : List<Product>
     {
+        
     }
 }
